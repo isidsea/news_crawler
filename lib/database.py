10a1abff42622fb99533a7667f1adc849174dfab
Frans <franssiswanto@gmail.com>
2016-10-11 16:25:28 +0700

    add new index

--- lib/database.py
+++ lib/database.py
@@ -6,10 +6,11 @@ class Database:
 
 	@classmethod
 	def get_db(self):
-		db = pymongo.MongoClient("mongodb://mongo:27017")
-		db = db["news_crawler"]
+		conn = pymongo.MongoClient("mongodb://mongo:27017")
+		db   = conn["news_crawler"]
 
 		# Ensuring Indexes
 		db.data.create_index([("permalink", pymongo.ASCENDING)], unique=True)
+		db.data.create_index([("converted", pymongo.ASCENDING)])
 		db.failed_urls.create_index([("permalink", pymongo.ASCENDING)], unique=True)
 		return db
