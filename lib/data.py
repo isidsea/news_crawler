e55a9230f3bf5967c86b1c6ce1bc19c3698fd761
Frans <franssiswanto@gmail.com>
2016-10-11 16:25:12 +0700

    add getting not converted log

--- lib/data.py
+++ lib/data.py
@@ -6,5 +6,6 @@ class Data:
 
 	@classmethod
 	def get_not_converted(self):
+		print("[data][debug] Getting not converted...")
 		db = Database.get_db()
 		return db.data.find({"$or":[{"converted":False}, {"converted":None}]})
